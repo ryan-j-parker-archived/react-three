cbc8b8b5aab27310d2f091e333308cfc60a5aa47
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-09-16 19:46:16 -0700

    three.js project init attempt

---
+++ src/three-app/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Main.css';
+
+import * as THREE from '../build/three.module.js';
+
+import {
+  PerspectiveCamera,
+  MeshStandardMaterial,
+  WebGLRenderer,
+} from '../build/three.module.js';
+
+export default function Main() {
+  return (
+    <div className="main-body"></div>
+  );
+}
